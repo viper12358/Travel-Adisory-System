694b0eac4f639b003d859ee17f99b4e101bc9a21
Adrian Tran <38680352+viper12358@users.noreply.github.com>
2020-03-06 16:43:55 -0700

    second

--- tas.py
+++ tas.py
@@ -1,4 +1,4 @@
 from app import app
 
-# if you want to check what's the secret key is, run python tasp.py
+# if you want to check what's the secret key is, run python tas.py
 print(app.config['SECRET_KEY'])
