6b98bb7b17047dca7e337070d4309a1b6927d3b0
szferguson <steven@datpixel.net>
2020-04-13 04:42:55 +0000

    Revert "hello"
    This reverts commit a71597b6c902b8d7e6432117237556fad884a28f.

--- config.py
+++ config.py
@@ -3,9 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'put-a-decent-cipher-here'
-    DISQUS_SECRET_KEY = 'Zei8rslSJzdqiT6caffCwbQkwkLru7fjpkaihXV6rqQWxWYyjAa68569zYCOJaea'
-    DISQUS_PUBLIC_KEY = 'Ikja2Kv5mSSw5Iwew0w1IgCB6RKzMqYaRUIXJjgCZs1Tn9SFv2BvLFW0cA0AXrTD'
-
 
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     #     'sqlite:///' + os.path.join(basedir, 'app.db')
